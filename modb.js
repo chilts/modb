46cac0363728c4991f5c44d82242c7e3a5719f7d
Andrew Chilton <andychilton@gmail.com>
2022-08-13 23:59:03 +1200

    Try and get some peer stuff working

--- modb.js
+++ modb.js
@@ -1,5 +1,7 @@
 // npm
 import fastify from 'fastify'
+import ms from 'ms'
+import got from 'got'
 
 // local
 import env from './lib/env.js'
@@ -7,6 +9,8 @@ import argv from './lib/argv.js'
 import Config from './lib/config.js'
 import Store from './lib/store.js'
 import createServerPeer from './lib/create-server-peer.js'
+import createServerApi from './lib/create-server-api.js'
+import * as task from './lib/task.js'
 
 // storage
 const config = new Config()
@@ -17,21 +21,34 @@ const peerServer = createServerPeer(env, argv, config, store)
 
 try {
   await peerServer.listen({ port: argv.peer.port })
-} catch (err) {
+}
+catch (err) {
   peerServer.log.error(err)
   process.exit(1)
 }
 
 // the API
-const apiApp = fastify({ logger: true })
-
-apiApp.get('/', async (request, reply) => {
-  return { ok: true }
-})
+const apiServer = createServerApi(env, argv, config, store)
 
 try {
-  await apiApp.listen({ port: argv.api.port })
-} catch (err) {
-  apiApp.log.error(err)
+  await apiServer.listen({ port: argv.api.port })
+}
+catch (err) {
+  apiServer.log.error(err)
   process.exit(1)
 }
+
+// firstly, let's connect to all 'seeds'
+console.log('argv:', argv)
+task.contactSeeds(argv.seedUrls, config)
+
+// connect to all peers regularly
+const peerInterval = ms('15s')
+function connectToAllPeers(config) {
+  setTimeout(
+    () => connectToAllPeers(config),
+    ( peerInterval / 2 ) + ( peerInterval * Math.random() )
+  )
+  task.contactPeers(config.get('peers'), config)
+}
+connectToAllPeers(config)
